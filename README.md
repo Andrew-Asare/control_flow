edd58b6ad3ee211b8dff3e6f6ecee3e1fc5e7c18
andrew <andrewasare98@gmail.com>
2021-03-24 11:36:33 +0000

    Control Flow

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
 # Control flow
 ##If, elf, if-elif and else
 ##For loops
-##While loops   
+##While loops
+##Loops help iterate through the data 
+- For loops
+- While loops
+- Data collections dict lists tuples strings
+###What are if statements?
+If statements are statements that utilize boolean conditional statements to decide upon the flow of
+code.
+
+'if' - The first part of an if statement that defines a condition for a result.
+'elif' - The second part which defines an alternate condition for an alternate result.
+'else' - The last part which runs only when no condition has been met.
+```python 
+if a == b:
+    print("a is equal to b")
+elif a > b:
+    print("a is greater than b")
+else:
+    print("a is none of the above")
+```
+##Loops
+###What are for loops?
+For loops are loops that are generally used to iterate through objects or collections of objects. They are
+also used when the number of times a loop should loop is known beforehand
+
+For use iterating through an object instead of explicit print statements
+
+```python
+shopping_list = ["bread", "eggs", "milk", "orange"]
+
+
+print(shopping_list)
+print(shopping_list[0])
+print(shopping_list[1])
+print(shopping_list[2])
+print(shopping_list[3])
+
+
+for items in shopping_list:
+    print(items)
+```
+
+Dictionaries can be looped through too:
+```python
+food_bill = {1: {"name": "James", "bill": "£1"}, 2: {"name": "Bond", "bill": "£2"},
+             3: {"name": "shah", "bill": "£3"}
+
+for items in food_bill.keys():
+    print(items)
+
+for items in food_bill.values():
+    print(f"{items['name']}'s total is: {items['bill']}")
+```
--- control_flow.py
+++ control_flow.py
@@ -1,13 +1,61 @@
-# Control flow
-# Conditional statements are used to control the flow of our program
+# # Control flow
+# # Conditional statements are used to control the flow of our program
+#
+# # age = 15
+# # # if the user is above the age of 15 allow them to buy a ticket
+# # if age >= 15: #If this condition is met/true the print statment will execute
+# #     print("Thank you, please proceed to your purchase")
+# #     #if the user is under the age of 15 do not allow to buy a ticket
+# # elif age < 15:
+# #     print("You are too young to purchase this ticket")
+# # # else block getes executed if non of the above conditions are met
+# # else:
+# #     print("Oops something went wrong, please try later")
+#
+# #Syntax to create a loop
+# # for is python keyword variable then data_collection
+#
+# shopping_list = ["bread", "eggs", "milk", "orange"]
+#
+#
+# print(shopping_list)
+# print(shopping_list[0])
+# print(shopping_list[1])
+# print(shopping_list[2])
+# print(shopping_list[3])
+#
+#
+# for items in shopping_list:
+#     print(items)
+#
+# # Let's iterate through letter in a word
+# # for letter in "fruits":
+# #     print(letter)
+# # shopping_list = ["bread", "eggs", "milk", "orange"]
+# #
+# # for items in shopping_list:
+# #     print(items)
+# #     if items == "milk": # when the condition is true the loop ends
+# #         # break is a key word
+# #         break # at this point when milk is found in items iterating through the shopping list the loop will stop
+# # Lets create a dict of a food bill so we can iterate using loops
+food_bill = {1: {"name": "James", "bill": "£1"}, 2: {"name": "Bond", "bill": "£2"},
+             3: {"name": "shah", "bill": "£3"}
+                              }
 
-age = 15
-# if the user is above the age of 15 allow them to buy a ticket
-if age >= 15: #If this condition is met/true the print statment will execute
-    print("Thank you, please proceed to your purchase")
-    #if the user is under the age of 15 do not allow to buy a ticket
-elif age < 15:
-    print("You are too young to purchase this ticket")
-# else block getes executed if non of the above conditions are met
-else:
-    print("Oops something went wrong, please try later")
+# for items in food_bill.keys():
+#     print(items)
+
+for items in food_bill.values():
+    print(f"{items['name']}'s total is: {items['bill']}")
+
+
+
+
+
+
+
+
+
+
+
